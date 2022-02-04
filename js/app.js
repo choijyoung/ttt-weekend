ddae1bb0d4958b9600b46d51aaff1dadbe8629e5
Jaeyoung Choi <choi.jae.y@gmail.com>
2022-02-04 13:45:02 -0500

    add cached element refs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
-/*---------------------------- Variables (state) ----------------------------*/
+// tiles within the tictactoe board.
 const tiles = ['#sq0', '#sq1', '#sq2', '#sq3,', '#sq4', '#sq5', '#sq6', '#sq7', '#sq8']
+// const players {
+//   player1: {
+    
+//   },
+//   player2: {
+
+//   }
+// };
+/*---------------------------- Variables (state) ----------------------------*/
+let isWinner, playerTurn, message
+
 // Message to notify whos turn it is.
 // We need to know which tile is chosen by which player
 // Wait until the turn changes before pressing another tile.
@@ -12,9 +20,19 @@ const tiles = ['#sq0', '#sq1', '#sq2', '#sq3,', '#sq4', '#sq5', '#sq6', '#sq7', 
 // if a player gets 3 in a row, if needs to display winner
 
 /*------------------------ Cached Element References ------------------------*/
-// const msg = document.querySelector('#message')
-const brd = document.querySelector('.board')
+const startBtn = document.getElementById('start-button')
+const resetBtn = document.getElementById('reset-button')
+const msgEl = document.getElementById('message')
 
+const topLeft = document.getElementById('sq0')
+const topMiddle = document.getElementById('sq1')
+const topRight = document.getElementById('sq2')
+const middleLeft = document.getElementById('sq3')
+const middle = document.getElementById('sq4')
+const middleRight = document.getElementById('sq5')
+const bottomLeft = document.getElementById('sq6')
+const bottomMiddle = document.getElementById('sq7')
+const bottomRight = document.getElementById('sq8')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
